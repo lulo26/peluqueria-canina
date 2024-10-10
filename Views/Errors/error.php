83630974ee21e8539338ad13af5c5dd174dc9e88
lulo <lopez.gallego2611@gmail.com>
2024-10-09 20:51:44 -0500

    pagina de error terminada

--- Views/Errors/error.php
+++ Views/Errors/error.php
@@ -1,13 +1,35 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
+  <head>
+  <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PAGINA NO ENCONTRADA</title>
-</head>
-<body>
-    <h1>Pagina no encontrada Error 404</h1>
-    <p>adlfkgjsdfklgjhdklgjhdsflgkjhdflkgjhsdfkjl</p>
-</body>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link href="<?= media() ?>/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link
+        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
+
+    <!-- Custom styles for this template-->
+    <link href="<?= media() ?>/css/sb-admin-2.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="Assets/css/ErrorPage.css">
+  </head>
+  <body>
+    <div class="container text-center">
+        <div class="row row-cols-1">
+            <div class="col"><h1>Error 404</h1></div>
+            <div class="col"><h3>Página no encontrada</h3></div>
+            <div class="col"><a class="btn btn-primary btn-icon-split btn-sm" href="<?= base_url()?>/home">
+                        <span class="icon text-white-50">
+                            <i class="fas fa-arrow-left"></i>
+                        </span>
+                        <span class="text">Volver al inicio</span>
+                        </a>
+                </div>
+        </div>
+        <img src="Assets/img/dog_vector.png" alt="">
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </body>
 </html>
