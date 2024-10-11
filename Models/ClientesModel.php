a2392e6ff40dfa1ee2cb1f91dc516425886bfbee
DiegoValdesR <bitardos8@gmail.com>
2024-10-11 17:01:09 -0500

    Validaciones registro clientes terminadas

--- Models/ClientesModel.php
+++ Models/ClientesModel.php
@@ -15,32 +15,29 @@ class ClientesModel extends Mysql{
         $this->usuario=$usuario;
         $this->password=$password;
 
-        $user_exist = "SELECT * from clientes Where usuario='{$this->usuario}'";
+        $sql=["SELECT * from clientes Where usuario='{$this->usuario}'","SELECT * from clientes where correo='{$this->correo}'"];
 
-        $cosa = $this->select_all($user_exist);
-        /* $sql=[,"SELECT * from clientes where correo='{$this->correo}'"];
- */
-        /* $array_request=[
+        $array_request=[
             "user"=>$this->select_all($sql[0]),
             "email"=>$this->select_all($sql[1])
         ];
- */
-        if (empty($cosa) /* || !empty($array_request["email"]) */) {
+
+        if (empty($array_request["user"]) && empty($array_request["email"])) {
 
             $query_insert = "INSERT INTO clientes(nombre,apellido,correo,telefono,usuario,`password`) VALUES(?, ?, ?, ?, ?, ?)";
             $arrData = array($this->nombre,$this->apellido,$this->correo,$this->telefono,$this->usuario,$this->password);
             $request_insert = $this->insert($query_insert, $arrData);
             $result = $request_insert;
             
-
-            /* if (!empty($array_request["user"])) {
-                $result="user exist";
-            } else {
-                $result="email exist";
-            } */
-            
         }else {
-            $result='exist';
+
+            if (empty($array_request["user"])) {
+                $result="email exist";
+            } elseif (empty($array_request["email"])) {
+                $result="user exist";
+            }else {
+                $result="both exist";
+            }
         }
 
         return $result;
