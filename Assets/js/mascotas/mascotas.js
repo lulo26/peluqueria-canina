ba99511c2cecc2a7af861bd489f26bc2c16f3b39
gooigi <lopez.gallego2611@gmail.com>
2024-10-17 11:31:10 -0500

    editar usuarios con errores falta arreglar el select

--- Assets/js/mascotas/mascotas.js
+++ Assets/js/mascotas/mascotas.js
@@ -1,30 +1,28 @@
 const frmMascotas = document.querySelector("#frmMascotas");
 const crearProductoModal = document.querySelector("#insertarMascotasModal");
 let nombreUsuario = document.querySelector("#nombreUsuario");
-const btnInsertarMascota = document.querySelector('#btnInsertarMascota');
+const btnInsertarMascota = document.querySelector("#btnInsertarMascota");
 let selectMascotas;
 let tablaMascotas;
 
-console.log("hello world");
-
+//console.log("hello world");
 
 document.addEventListener("DOMContentLoaded", () => {
-
-  btnInsertarMascota.addEventListener('click', (e) =>{
+  btnInsertarMascota.addEventListener("click", (e) => {
     //traigame los datos del select
+    nombreUsuario.innerHTML = `<option value="0" selected hidden>seleccione el dueño</option>`;
     fetch(base_url + "/clientes/getClientes")
-      .then((res)=>res.json())
+      .then((res) => res.json())
       .then((data) => {
-        data.forEach(el => {
-          nombreUsuario.innerHTML += `<option value="${el.idClientes}">${el.nombre}</option>`
-
+        data.forEach((el) => {
+          nombreUsuario.innerHTML += `<option value="${el.idClientes}">${el.nombre}</option>`;
         });
-      })
-      document.querySelector('#idMascotas').value = 0
-        frmMascotas.reset()
-        $('#insertarMascotasModal').modal('show')
-        document.getElementById('MascotaModalLabel').innerHTML = "Crear Mascota"
-  })
+      });
+    document.querySelector("#idMascotas").value = 0;
+    frmMascotas.reset();
+    $("#insertarMascotasModal").modal("show");
+    document.getElementById("MascotaModalLabel").innerHTML = "Crear Mascota";
+  });
 
   tablaMascotas = $("#tablaMascotas").dataTable({
     language: {
@@ -35,28 +33,28 @@ document.addEventListener("DOMContentLoaded", () => {
       dataSrc: "",
     },
     columns: [
-      {"data":"nombreMascota" },
-      {"data":"razaMascota" },
-      {"data":"edadMascota" },
-      {"data":"comentarioMascota" },
-      {"data":"nombre" },
-      {"data":"options"}
+      { data: "nombreMascota" },
+      { data: "razaMascota" },
+      { data: "edadMascota" },
+      { data: "comentarioMascota" },
+      { data: "nombre" },
+      { data: "options" },
     ],
     responsive: "true",
     order: [[0, "asc"]],
   });
 
-  
-
   frmMascotas.addEventListener("submit", (e) => {
     e.preventDefault();
     frmData = new FormData(frmMascotas);
+    console.log(frmData);
     fetch(base_url + "/mascotas/setMascotas", {
       method: "POST",
       body: frmData,
     })
       .then((res) => res.json())
-      .then((data) => {Swal.fire({
+      .then((data) => {
+        Swal.fire({
           title: data.status ? "Correcto" : "Error",
           text: data.msg,
           icon: data.status ? "success" : "error",
@@ -69,63 +67,66 @@ document.addEventListener("DOMContentLoaded", () => {
       });
   });
 
-  document.addEventListener('click', (e)=>{
+  document.addEventListener("click", (e) => {
     try {
-        let selected = e.target.closest('button').getAttribute('data-action-type')
-        let idMascotas = e.target.closest('button').getAttribute('rel')
-        
-        //Eliminar mascota
-        if (selected == 'delete') {
-            Swal.fire({
-                title:"Eliminar Mascota",
-                text:"¿Está seguro de eliminar la mascota?",
-                icon: "warning",
-                showDenyButton: true,
-                confirmButtonText: "Sí",
-                denyButtonText: `Cancelar`
-            }).then((result) => {
-                if (result.isConfirmed) {
-                    let formData = new FormData()
-                    formData.append('idMascotas', idMascotas)
-                    fetch(base_url + '/mascotas/eliminarMascota',{
-                        method: "POST",
-                        body: formData,
-                    })
-                    .then((res)=>res.json())
-                    .then((data)=>{
-                        Swal.fire({
-                            title: data.status ? 'Correcto' : 'Error',
-                            text: data.msg,
-                            icon: data.status ? "success" : 'error'
-                        })
-                        tablaMascotas.api().ajax.reload(function(){})
-                    })
-                }
+      let selected = e.target
+        .closest("button")
+        .getAttribute("data-action-type");
+      let idMascotas = e.target.closest("button").getAttribute("rel");
+
+      //Eliminar mascota
+      if (selected == "delete") {
+        Swal.fire({
+          title: "Eliminar Mascota",
+          text: "¿Está seguro de eliminar la mascota?",
+          icon: "warning",
+          showDenyButton: true,
+          confirmButtonText: "Sí",
+          denyButtonText: `Cancelar`,
+        }).then((result) => {
+          if (result.isConfirmed) {
+            let formData = new FormData();
+            formData.append("idMascotas", idMascotas);
+            fetch(base_url + "/mascotas/eliminarMascota", {
+              method: "POST",
+              body: formData,
+            })
+              .then((res) => res.json())
+              .then((data) => {
+                Swal.fire({
+                  title: data.status ? "Correcto" : "Error",
+                  text: data.msg,
+                  icon: data.status ? "success" : "error",
+                });
+                tablaMascotas.api().ajax.reload(function () {});
               });
-
-        }
-        //Actualizar producto
-        if (selected == 'update') {
-            $('#insertarMascotasModal').modal('show')
-            document.getElementById('MascotaModalLabel').innerHTML = "Actualizar Mascota"
-            fetch(base_url + `/mascotas/getMascota/${idMascotas}`,{
-                method: "GET"
-            })
-            .then((res)=>res.json())
-            .then((res)=>{
-                arrData = res.data[0]
-                console.log(arrData)
-                document.querySelector('#nombreMascota').value = arrData.nombreMascota
-                res.forEach(el => {
-                  nombreUsuario.innerHTML += `<option value="${el.idClientes}">${el.nombre}</option>`
-                })
-                document.querySelector('#razaMascota').value = arrData.razaMascota
-                document.querySelector('#edadMascota').value = arrData.edadMascota
-                document.querySelector('#comentarioMascota').value = arrData.comentarioMascota
-                document.querySelector('#idMascotas').value = arrData.idMascotas
-            })
-        }
-    }catch{}
-})
-
+          }
+        });
+      }
+      //Actualizar producto
+      if (selected == "update") {
+        $("#insertarMascotasModal").modal("show");
+        document.getElementById("MascotaModalLabel").innerHTML =
+          "Actualizar Mascota";
+        fetch(base_url + `/mascotas/getMascota/${idMascotas}`, {
+          method: "GET",
+        })
+          .then((res) => res.json())
+          .then((res) => {
+            arrData = res.data[0];
+            console.log(arrData);
+            document.querySelector("#nombreMascota").value =
+              arrData.nombreMascota;
+            document.querySelector("#razaMascota").value = arrData.razaMascota;
+            document.querySelector("#edadMascota").value = arrData.edadMascota;
+            document.querySelector("#comentarioMascota").value =
+              arrData.comentarioMascota;
+            document.querySelector("#idMascotas").value = arrData.idMascotas;
+            res.forEach((el) => {
+              nombreUsuario.innerHTML += `<option value="${el.idClientes}">${el.nombre}</option>`;
+            });
+          });
+      }
+    } catch {}
+  });
 });
