ff4835e5968501fa23d091e079da77db6599e957
SENA <SENA@CARAPRCTIFSD503.FormacionSena.RED>
2024-10-17 09:43:37 -0500

    Agregando boton para llevar hacia arriba home

--- Views/home.php
+++ Views/home.php
@@ -397,7 +397,8 @@
   <!-- footer section -->
 
    <!-- Back to Top -->
-   <a href="#" class="btn btn-lg btn-primary btn-lg-square back-to-top"><i class="fa fa-angle-double-up"></i></a>
+   <a href="#" class="btn btn-lg btn-primary btn-lg-square back-to-top"><i class="fa fa-angle-double-up"></i>
+  <img src="Assets/img/home/flecha_arriba.png" alt=""></a>
 
 
 
